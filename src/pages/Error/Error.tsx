24d2e379ec5003e0bc42f730a97542fed60ab1a4
urke90 <you@example.com>
2023-11-25 19:17:36 +0100

    react router setup. Created routes and it's components with dummy data

---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,7 @@
+interface IErrorProps {}
+
+const Error: React.FC<IErrorProps> = (props) => {
+  return <div>Error Page</div>;
+};
+
+export default Error;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+interface IHomeProps {}
+
+export const Home: React.FC<IHomeProps> = (props) => {
+  return (
+    <div>
+      <h1>Welcome dear customers</h1>
+    </div>
+  );
+};
+
+export default Home;
